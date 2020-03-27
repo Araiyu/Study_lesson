54176d630ee48d9fab07e5c0bfb58d9e28a30a46
新井　雄介 <ay088055@gmail.com>
2020-03-27 19:47:48 +0900

    rails_8_finish

---
+++ sample_app2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '68ab353e69a2dbe1e1b3adb07d9c5207cbcb5047db71b3eb18c7596239f1528528ff7aef31cfcb7f4548db8b1ce938b6dca98cda60e8faab692343107886de66'
